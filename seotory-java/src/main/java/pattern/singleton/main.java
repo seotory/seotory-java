d6f19dab0f3f9aec966b786a6829cb57111ab778
axsusia <zion437@gmail.com>
2015-09-25 11:35:54 +0900

    name cg, ex source cg

--- seotory-java/src/main/java/pattern/singleton/main.java
+++ seotory-java/src/main/java/pattern/singleton/main.java
@@ -9,25 +9,39 @@ import java.io.ObjectOutputStream;
 
 public class main {
 	
-	//static EagerInitialization instance = EagerInitialization.getInstance();
-	static BillPushInitializtion instance = BillPushInitializtion.getInstance();
+	static EagerInitialization instance = EagerInitialization.getInstance();
+	//static BillPushInitializtion instance = BillPushInitializtion.getInstance();
 	
 	public static void main(String[] args) throws Exception {
-		SerializedInitializtion instance = SerializedInitializtion.getInstance();
+		InitializationOnDemandHolderIdiom instance = InitializationOnDemandHolderIdiom.getInstance();
+//		EagerInitialization instance22 = EagerInitialization.getInstance();
+//		EagerInitialization instance24 = EagerInitialization.getInstance();
+//		StaticBlockInitalization instance33 = StaticBlockInitalization.getInstance();
+//		StaticBlockInitalization instance34 = StaticBlockInitalization.getInstance();
+//		SerializedInitializtion instance = SerializedInitializtion.getInstance();
 		
-		String filepath = "d:/test.ser";
-		
-		ObjectOutput out = new ObjectOutputStream(new FileOutputStream( filepath ));
-		out.writeObject(instance);
-		out.close();
-		
-		ObjectInput in = new ObjectInputStream(new FileInputStream( filepath ));
-		SerializedInitializtion instance2 = (SerializedInitializtion)in.readObject();
-		
-		System.out.println( instance.hashCode() );
-		System.out.println( instance2.hashCode() );
+//		String filepath = "d:/test.ser";
+//		
+//		ObjectOutput out = new ObjectOutputStream(new FileOutputStream( filepath ));
+//		out.writeObject(instance);
+//		out.close();
+//		
+//		ObjectInput in = new ObjectInputStream(new FileInputStream( filepath ));
+//		SerializedInitializtion instance2 = (SerializedInitializtion)in.readObject();
+//		
+//		System.out.println( instance.hashCode() );
+//		System.out.println( instance2.hashCode() );
+		synchronized (java.lang.Object.class) {
+	        System.out.println("===========µð¹ö±ë ½ÃÀÛÇß´Ù~================");
+	        System.out.println("time:"
+	                + new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date()));
+	        System.out.print(new Throwable().getStackTrace()[0].getClassName() + "."
+	                + new Throwable().getStackTrace()[0].getMethodName() + "()");
+	        System.out.println("  line: " + new Throwable().getStackTrace()[0].getLineNumber());
+	        //System.out.println(something);
+	        System.out.println("===========µð¹ö±ë ³¡³µ´Ù~================");
+	    }
 	}
-	
 }
 
 class ThreadEx extends Thread {
