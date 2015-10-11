bfc8e3bb28194e1d764d1822f1dfbb2f017cc3c2
seotory <seotory437@gmail.com>
2015-10-11 11:30:40 +0900

    add bulder pattern

---
+++ seotory-java/src/main/java/pattern/builder/Product.java
@@ -0,0 +1,50 @@
+package pattern.builder;
+
+/**
+ * Created by zion437 on 2015. 10. 11..
+ */
+public class Product {
+    private String name;
+    private int price;
+
+    private boolean isSell;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public boolean isSellEnabled() {
+        return isSell;
+    }
+
+    private Product(ProductBuilder builder) {
+        this.name = builder.name;
+        this.price = builder.price;
+        this.isSell = builder.isSell;
+    }
+
+    public static class ProductBuilder {
+        private String name;
+        private int price;
+        private boolean isSell;
+
+        public ProductBuilder(String name, int price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        public ProductBuilder setIsSellEnabled(boolean isSell) {
+            this.isSell = isSell;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
+
+    }
+}
